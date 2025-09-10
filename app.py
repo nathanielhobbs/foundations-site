64aec8a32d1357905fcfb3bf1d50d2f81276f624
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2025-09-10 19:25:10 +0000

    moved previous home page to About page and updated home page to show course schedule and what we've done so far section

--- app.py
+++ app.py
@@ -595,6 +595,10 @@ def ssh():
 def bash():
     return render_template("bash.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 @app.route("/")
 def index():
     return render_template("index.html")
